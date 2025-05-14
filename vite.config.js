7866455137653841b0ccc4ddcac37c4ef1f0a1a9
= <=>
2025-05-14 14:24:23 +0700

    First commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+    // Đường dẫn gốc khi deploy (gh-pages cần đúng path)
+    base: "/f8-zoom-module-1",
+
+    // Thư mục chính chứa HTML, SCSS, JS
+    root: "src",
+
+    // Thư mục chứa các file tĩnh như ảnh, fonts, v.v.
+    publicDir: "../public",
+
+    build: {
+        // Nơi xuất ra sản phẩm sau khi build
+        outDir: "../dist",
+        emptyOutDir: true,
+
+        // Khai báo các trang cần build (nhiều trang)
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, "src/index.html"),
+                account: path.resolve(__dirname, "src/account.html"),
+                // Thêm trang khác nếu có
+            },
+        },
+    },
+
+    plugins: [ViteEjsPlugin()],
+});
